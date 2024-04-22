d4744fabf9d492dcd45a7d6e9ee5f254d19b53e2
StaffEngineer <velo.app1@gmail.com>
2024-04-22 14:55:04 +0100

    Update

--- src/keys.el
+++ src/keys.el
@@ -37,8 +37,8 @@
   (interactive)
   (let ((term (thing-at-point 'symbol t))) (occur term)))
 
-(define-key evil-normal-state-map  (kbd "C-r")         #'swiper)
-(define-key my-keys-minor-mode-map (kbd "C-s")         #'swiper-thing-at-point)
+(define-key evil-normal-state-map  (kbd "C-s")         #'swiper)
+(define-key my-keys-minor-mode-map (kbd "C-r")         #'swiper-thing-at-point)
 (define-key my-keys-minor-mode-map (kbd "C-c )")       #'kmacro-end-macro)
 (define-key my-keys-minor-mode-map (kbd "C-c (")       #'kmacro-start-macro)
 (define-key my-keys-minor-mode-map (kbd "C-c ,")       #'kmacro-end-and-call-macro)
